b5ccbf425f425379f5618fd01c84d53e91b13287
jdalpert <josh1994bv@gmail.com>
2016-04-05 15:37:12 -0700

    Added new SQL commands

--- sql_commands.sql
+++ sql_commands.sql
@@ -26,3 +26,19 @@ VALUES ('title', 'value', 'value', 'value', 'value' 'value');
 -- deletes a movie from the movie table 
 
 DELETE FROM movies WHERE year = 2017
+
+-- finds all duplicate first names
+
+select s.first_name as 'first', count(distinct s.last_name) as cnt
+from stars_in_movies as sm, stars as s, movies as m
+where sm.star_id = s.id and m.id = sm.movie_id
+group by s.first_name
+having (cnt > 1);
+
+-- finds all duplicate last_name
+
+select s.last_name as 'last', count(distinct s.first_name) as cnt
+from stars_in_movies as sm, stars as s, movies as m
+where sm.star_id = s.id and m.id = sm.movie_id
+group by s.last_name
+having (cnt > 1);
