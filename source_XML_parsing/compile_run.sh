50b8329558eb7082ad8dbf0bcb323fdfc241fb92
jangelsb <jangelsb.vcs@gmail.com>
2016-05-05 18:14:08 -0700

    removed variables lol

--- source_XML_parsing/compile_run.sh
+++ source_XML_parsing/compile_run.sh
@@ -1,17 +1,11 @@
 #! /bin/bash
 
-MYSQL_USERNAME="classta"
-MYSQL_PASSWORD="classta"
-XML_MOVIES="./mains243.xml"
-XML_STARS="./actors63.xml"
-XML_CAST="./casts124.xml"
-
 echo "Creating Tables"
-mysql --user="$MYSQL_USERNAME" --password="$MYSQL_PASSWORD" < createtable.sql
+mysql --user="classta" --password="classta" < createtable.sql
 echo "COMPLETE"
 echo "Compiling Java Resources"
 javac ./src/*.java -d ./bin/
 echo "COMPLETE"
 echo "Running XML Parser"
-java -classpath ./bin/:./lib/mysql-connector-java-5.0.8-bin.jar Parser $XML_MOVIES $XML_STARS $XML_CAST
+java -classpath ./bin/:./lib/mysql-connector-java-5.0.8-bin.jar Parser "./mains243.xml" "./actors63.xml" "./casts124.xml"
 echo "COMPLETE"
