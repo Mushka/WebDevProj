dc5c941d8a77e787d953ab9ec26c291a03d3598b
Ubuntu <ubuntu@ip-172-31-23-40.us-west-2.compute.internal>
2016-05-06 01:20:27 +0000

    fixed bash scripts

--- source_XML_parsing/clean.sh
+++ source_XML_parsing/clean.sh
@@ -1,13 +1,8 @@
 #! /bin/bash
 
-MYSQL_USERNAME="classta"
-MYSQL_PASSWORD="classta"
-DATABASE_NAME="moviedb_project3_grading"
-
 echo "Removing Class Files"
-rm ./bin/*.class
+rm -f ./bin/*.class
 echo "COMPLETE"
-echo "Dropping Database: $DATABASE_NAME"
-mysql --user="$MYSQL_USERNAME" --password="$MYSQL_PASSWORD" -e 'DROP DATABASE IF EXISTS $DATABASE_NAME'
+echo "Dropping Database: moviedb_project3_grading"
+mysql --user="classta" --password="classta" -e 'DROP DATABASE IF EXISTS moviedb_project3_grading;' 
 echo "COMPLETE"
-
