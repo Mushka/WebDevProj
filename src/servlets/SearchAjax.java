771452db8fe73edcc3baaa2b54619cd03dc7fcf9
jdalpert <josh1994bv@gmail.com>
2016-05-14 18:46:28 -0700

    Implemented Ajax tokenized search, does not print to screen but prints to console

--- src/servlets/SearchAjax.java
+++ src/servlets/SearchAjax.java
@@ -129,9 +129,13 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.Map.Entry;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -179,8 +183,6 @@ public class SearchAjax extends HttpServlet {
 			String lName = request.getParameter("last_name");
 
 			String advance =  request.getParameter("adv");
-			
-//			System.out.println(advance);
 					
 			if(limit==null)
 				limit = "10";
@@ -193,83 +195,91 @@ public class SearchAjax extends HttpServlet {
 
 		String query = "";
 		ArrayList<String> queryList = new ArrayList<String>();
-
-		if(advance == null)
-		{
-			for(String s : title.split(" "))
-				queryList.add("SELECT * FROM movies WHERE title like '%"+s+"%'");			
-		}
-		else
-		{
+		String wordsToSearch[] = title.split(" ");
+		for(int k = 0; k < wordsToSearch.length; k++){	
+			if(advance == null || "".equals(wordsToSearch) || (wordsToSearch.length == 1 && wordsToSearch[0].length() == 1))
+			{
+				query = "SELECT * FROM movies WHERE title like '"+title+"%'";
+			}
+			else
+			{	
+				if((k  != wordsToSearch.length - 1)||(wordsToSearch.length == 1)){
+					query = "SELECT distinct m.id, title, year, director, banner_url, trailer_url "
+							+ "FROM movies as m, stars_in_movies as sm, stars as s "
+							+ "WHERE sm.star_id = s.id AND m.id = sm.movie_id "
+							+ "AND m.title like '%" + wordsToSearch[k] + "%'";
+				}else{
+					query = "SELECT distinct m.id, title, year, director, banner_url, trailer_url "
+							+ "FROM movies as m, stars_in_movies as sm, stars as s "
+							+ "WHERE sm.star_id = s.id AND m.id = sm.movie_id "
+							+ "AND m.title like '% " + wordsToSearch[k] + "%'";
+				}
+				if (year != null)
+					query += " AND m.year like '%" + year + "'";
+				if (director != null)
+					query += " AND m.director like '%" + director + "%'";
+				if (fName != null) 
+					query += " AND s.first_name like '%" + fName + "%'";
+				if (lName != null)
+					query += " AND s.last_name like '%" + lName + "%'";
+			}
 			
-			query = "SELECT distinct m.id, title, year, director, banner_url, trailer_url "
-					+ "FROM movies as m, stars_in_movies as sm, stars as s "
-					+ "WHERE sm.star_id = s.id AND m.id = sm.movie_id "
-					+ "AND m.title like '%" + title + "%'";
-
-			if (year != null)
-				query += " AND m.year like '%" + year + "'";
-			if (director != null)
-				query += " AND m.director like '%" + director + "%'";
-			if (fName != null) 
-				query += " AND s.first_name like '%" + fName + "%'";
-			if (lName != null)
-				query += " AND s.last_name like '%" + lName + "%'";
-		}
-
-
+			switch(orderby)
+			{
+			default:
+				query += " order by title";
+				break;
+			case "desc_t":
+				query += " order by title DESC";
+				break;
+			case "asc_y":
+				query += " order by year, title";
+				break;
+			case "desc_y":
+				query += " order by year DESC, title";
+				break;
+			}
+			queryList.add(query);
+    	}
+		query += " LIMIT "+ limit +" OFFSET "+offset;
 		String count_query = query;
 		
-		switch(orderby)
-		{
-		default:
-			query += " order by title";
-			break;
-		case "desc_t":
-			query += " order by title DESC";
-			break;
-		case "asc_y":
-			query += " order by year, title";
-			break;
-		case "desc_y":
-			query += " order by year DESC, title";
-			break;
-		}
-		
-		query += " LIMIT "+ limit +" OFFSET "+offset;
-		
         List<Movie> movies = new ArrayList<Movie>();
-        List<Movie> allMovies = null;
-        if(advance == null){
-        	allMovies = Movie.getMovies(queryList.get(0));
-        	for(int i = 1; i < queryList.size(); i++ ){
-        		List<Movie> copyMovies = new ArrayList<Movie>(allMovies);
-        		List<Movie> nextWord = Movie.getMovies(queryList.get(i));
-        		copyMovies.removeAll(nextWord);
-        		allMovies.removeAll(copyMovies);
-        		System.out.println("WTF");
+        HashMap<String, Movie>  allMovies = new HashMap<String, Movie>();
+        if(advance != null|| (!("".equals(wordsToSearch[0]))) || (wordsToSearch.length != 1 && wordsToSearch[0].length() != 1)){
+    		for(Movie m : Movie.getMovies(queryList.get(0)))
+    			allMovies.put(m.toString(), m);
+        	for(int i = 1; i < queryList.size(); i++){
+                HashMap<String, Movie> copyMovies = new HashMap<String, Movie>(allMovies);	
+                HashMap<String, Movie> nextWord = new HashMap<String, Movie>();
+        		for(Movie m : Movie.getMovies(queryList.get(i)))
+        			nextWord.put(m.toString(), m);
+        		for(Entry<String, Movie> m : nextWord.entrySet())
+        			copyMovies.remove(m.getKey());   		
+        		for(Entry<String, Movie> m : copyMovies.entrySet())
+        			allMovies.remove(m.getKey());
         	}
-        	for(int j = Integer.parseInt(offset); j < Integer.parseInt(limit) + Integer.parseInt(offset); j++){
+        	SortedSet<String> sortedMovies = new TreeSet<String>(allMovies.keySet());
+        	ArrayList<String> movieFinder = new ArrayList<String>(sortedMovies);
+        	for(int j = Integer.parseInt(offset); j < ((Integer.parseInt(limit) + Integer.parseInt(offset))-1); j++){
         		try{
-        			movies.add(allMovies.get(j));
+        			movies.add(allMovies.get(movieFinder.get(j)));
         		}catch(Exception e){
+        			System.out.print("Wtf " + j);
         			break;
         		}
         	}
         }else
         	movies = Movie.getMovies(query);
-        
-        if(advance == null)
+        if(advance == null || "".equals(wordsToSearch[0]) || (wordsToSearch.length == 1 && wordsToSearch[0].length() == 1))
     		count_query = "SELECT COUNT(*) as count " + count_query.substring(count_query.indexOf("FROM"));
-        else
-    		count_query = "SELECT COUNT(distinct m.id) as count " + count_query.substring(count_query.indexOf("FROM"));
-
-        String num_of_movies = "";
-		if(advance == null)
-			num_of_movies = Integer.toString(movies.size());
-		else
-			num_of_movies = MySQL.select(count_query).get(0).get("count").toString();
-//		System.out.println(num_of_movies);
+        
+        String num_of_movies =  "";
+        if(advance == null || "".equals(wordsToSearch[0]) || (wordsToSearch.length == 1 && wordsToSearch[0].length() == 1))
+        	num_of_movies = MySQL.select(count_query).get(0).get("count").toString();
+        else 
+        	num_of_movies = Integer.toString(allMovies.size());
+        
         request.getSession().setAttribute("movies", movies);
         request.getSession().setAttribute("offset", offset);
         request.getSession().setAttribute("limit", limit);
@@ -282,7 +292,6 @@ public class SearchAjax extends HttpServlet {
 		request.getSession().setAttribute("director", director);
 		request.getSession().setAttribute("first_name", fName);
 		request.getSession().setAttribute("last_name", lName);
-
 		request.getSession().setAttribute("adv", advance);
 
         out.print(new Gson().toJson(movies));
