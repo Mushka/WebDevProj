96b2cf78e728d0d5a513f04d77d0efb450ebeb21
jangelsb <joshangelsberg@gmail.com>
2016-05-20 22:04:52 -0700

    updated info to match aws's

--- source_XML_parsing_md/src/Credentials.java
+++ source_XML_parsing_md/src/Credentials.java
@@ -1,6 +1,6 @@
 public class Credentials {
 	
 	public static String admin = "root";
-	public static String password = "password12";
+	public static String password = "password234";
 	public static String url = "jdbc:mysql://localhost:3306/moviedb";
 }
