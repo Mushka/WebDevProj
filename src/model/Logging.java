310b5f148be3cc16fd300df1b3e04eb9bdcbf813
jangelsb <jangelsb.vcs@gmail.com>
2016-06-04 23:10:59 -0700

    connection pooling, prepared statements, done

--- src/model/Logging.java
+++ src/model/Logging.java
@@ -4,6 +4,11 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.EnumSet;
+
+import java.nio.file.attribute.PosixFilePermission;
 
 public class Logging {
 	public static void appendLogTS(String value) {
@@ -17,7 +22,13 @@ public class Logging {
 	public static void appendLog(String value) {
 		try {
 
-	    	File file = new File("/Users/Max/Development/cs122b/FabFlix/logFile.txt");
+			
+			Files.setPosixFilePermissions(Paths.get(System.getProperty("user.home")), 
+				    EnumSet.of(PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE, PosixFilePermission.OWNER_EXECUTE, PosixFilePermission.GROUP_READ, PosixFilePermission.GROUP_EXECUTE));
+			
+//	    	File file = new File(getServletContext().getRealPath("/") + "logFile.txt");
+
+	    	File file = new File(System.getProperty("user.home") + "/logFile.txt");
 
 	    	if(!file.exists()){
 	    	   file.createNewFile();
